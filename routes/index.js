a85336b6178dc9a14e0938899e8b8c9841990d99
Hunar Khanna <hunarkhanna@104-226.priv23.nus.edu.sg>
2012-10-18 14:43:04 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
-exports.hunar = function (argument) {
+exports.hunar = function (req, res) {
 	// body...
+  res.render('indes')
 }
