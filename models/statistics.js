370a7e2c0478974b3de0fd3bb98cc5bc366a6da1
hunar <hunur.khanna@gmail.com>
2012-11-16 11:46:33 +0800

    Changes to statistics model made

--- models/statistics.js
+++ models/statistics.js
@@ -11,13 +11,14 @@ var connection = sql.createConnection({
 exports.lastWeekPerformance = function (args, callback) {
 	var outlet_id = args.outlet_id;
 	console.log(outlet_id);
-	if(outlet_id!=null) {
-		var query = 'SELECT p.name as name, r.revenue as revenue, r.date as date from revenue r inner join product p on p.barcode'+
+	if(outlet_id!==null) {
+		var query = 'SELECT p.name as name, r.revenue as revenue, DATE_FORMAT(r.date,\'%Y-%m-%d\') as date from revenue r inner join product p on p.barcode'+
 					' = r.barcode WHERE r.outlet_id=' +outlet_id+' AND r.date>=SUBDATE(CURDATE(),7);';
 
 		connection.query(query, function(err, rows, fields) {
 			if(!err) {
 				console.log("Retrieval successful!");
+				console.log(rows);
 				callback(null,rows);
 			} else {
 				console.log("ERROR encountered : " + err);
@@ -33,7 +34,7 @@ exports.lastWeekPerformance = function (args, callback) {
 exports.allOutletsRevenue = function(args,callback) {
 	var query = 'SELECT o.s_name as name, r.revenue as revenue, FORMAT((r.revenue / t.total)*100,0) as percent'+
 				' from outlet o,revenue r,(SELECT SUM(revenue) as total from revenue where date=SUBDATE(CURDATE(),1)) t '+
-				' where o.id=r.outlet_id;';
+				' where o.id=r.outlet_id and r.date=SUBDATE(CURDATE(),1) ;';
 
 	connection.query(query, function(err,rows,fields) {
 		if(!err) {
