e03df9e3f32293017ee61ca28b466e152e0009e6
Pei Yi <peiyiru@gmail.com>
2012-11-30 02:31:34 +0800

    cw view

--- views/js/website.js
+++ views/js/website.js
@@ -1,14 +1,18 @@
+var global;
+
 $().ready(function(){
 $('#catalog-display').append('<ul id="catalog-list" class="thumbnails">');
 
-for (var i = 0; i < 6; i++){
-$('#catalog-display').append('<li class="span3" style="padding-bottom: 20px;"><div class="thumbnail">'
+$.getJSON('/website/viewProducts', function(data){
+	$.each(data,function(k,v){
+		$('#catalog-display').append('<li class="span3" style="padding-bottom: 20px;"><div class="thumbnail">'
 		+'<a href="#">'
 			+'<img src="http://placehold.it/300x200" alt="">'
 		+'</a>'
-		+'<div class="caption"><h3>Thumbnail label</h3>'
-		+'<p>Thumbnail caption...</p></div>'
+		+'<div class="caption"><h3>'+v.name+'</h3>'
+		+'<p>Category: '+v.category+'<br/>Manufacturer: '+v.manufacturer+'<br />Stock Availability: '+v.stock+'<br/>Price: $'+v.selling_price+'</p></div>'
 		+'</div></li>');
-}
+	});
+});
 $('#catalog-display').append('</ul>');	
 })
